5563ccc973699a0915904aab92a40bf60f2c53d1
Anand Agrawal <anand@technogise.com>
2017-05-10 16:22:33 +0530

    my activity

---
+++ app/src/main/java/com/technogise/jenkindemo/MainActivity.java
@@ -0,0 +1,15 @@
+package com.technogise.jenkindemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //vsdvfvsdf
+    }
+}
